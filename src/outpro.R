15e8fb8e07aaba756f5b0a2cf47c075895348a68
Pengwei Hu <101491675+1027victor@users.noreply.github.com>
2024-05-16 20:45:59 +0800

    Update outpro.R

--- src/outpro.R
+++ src/outpro.R
@@ -6,13 +6,20 @@ tmp = args[2]
 outdir = args[3]
 timestamp = args[4]
 library(data.table)
-load(paste(tmp,"/original.RData", sep = ""))
+# load(paste(tmp,"/original.RData", sep = ""))
 file = paste(tmp,"/scIGANs-", job_name,".csv", sep = "")
 
 d <- fread(file, header = T)
+# new add
 # gcm 行是细胞，列是基因
-gcm <- d[,-1]*reads_max_cell
+# 进行反归一化
+# gcm <- t(apply(d, 1, function(x) {
+#   original_data <- x * (reads_max_cell - reads_min_cell) + reads_min_cell
+#   return(original_data)
+# }))
 
+# gcm <- d[,-1]*reads_max_cell
+gcm<-d[,-1]
 gcm_out <- cbind(Gene_ID = genenames, t(gcm[,1:geneCount]))
 colnames(gcm_out) = cellnames
 
